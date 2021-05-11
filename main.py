211a402aa4098aaf3fa882c6667cd9807e6aee54
Sjur Espedal <sjurbes@gmail.com>
2021-05-11 15:28:50 +0100

    add first flask endpoint

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/")
+def index():
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    app.run(port=8080, debug=True)
